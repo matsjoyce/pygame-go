eec0e6043555ad6423870bf777aeb5e1a899550e
Matthew Joyce <matsjoyce@gmail.com>
2017-01-19 09:47:10 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import pathlib
+from distutils.core import setup
+
+setup(name="pygame-quick",
+      version="0.1",
+      author="Matthew Joyce",
+      author_email="matsjoyce@gmail.com",
+      description="A simplified version of pygame for use in teaching",
+      url="https://github.com/matsjoyce/pygame-quick",
+      download_url="https://github.com/matsjoyce/pygame-quick/releases",
+      packages=["pygame_quick"],
+      classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Environment :: X11 Applications",
+            "Intended Audience :: Education",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.0",
+            "Programming Language :: Python :: 3.1",
+            "Programming Language :: Python :: 3.2",
+            "Programming Language :: Python :: 3.3",
+            "Programming Language :: Python :: 3.4",
+            "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3 :: Only",
+            "Topic :: Education",
+            "Topic :: Games/Entertainment",
+            "Topic :: Multimedia :: Graphics"
+            "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+            ]
+      )
